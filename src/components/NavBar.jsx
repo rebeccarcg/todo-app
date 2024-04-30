4bd57fefee879af0360270a80bfed4d30d446521
rebeccarcg <rebecca.reimers@chasacademy.se>
2024-04-30 19:04:06 +0200

    Style navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,10 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav>
-      <div>
-        <ul>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/todo">ToDo</NavLink>
-          <NavLink to="/about">About</NavLink>
-        </ul>
+      <div className="navbar">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/todo">To Do</NavLink>
+        <NavLink to="/about">About</NavLink>
       </div>
     </nav>
   );
